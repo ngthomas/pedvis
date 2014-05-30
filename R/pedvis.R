443c1f091da172169f024a39fb34c0c4d1fe6ce7
Eric C. Anderson <eric.anderson@noaa.gov>
2014-05-30 12:15:42 -0700

    Add stuff to make prong nodes dashed.

--- R/pedvis.R
+++ R/pedvis.R
@@ -31,25 +31,40 @@ simple_test_ped <- function() {
 #' genotype data associated with them.
 #' @param ShowLabelNodes a character vector of the identifiers of the kids, mas, and pas that should have their
 #' labels printed inside their nodes.
+#' @param ProngNodes a character vector of the identifiers of the kids, mas, and pas that should be considered
+#' prong nodes, and therefore be printed a little differently.  Additionally, the edges incident to a prong node
+#' will be drawn with ProngEdgeStyle.
 #' @param ObsStyle a named list of the extra style values that you want the observed nodes to have
-#' @param outf Name of the output file (so files will be outf.dot, outf.ps, outf.pdf, etc.)
+#' @param ProngStyle a named list of the extra style values that you want the prong nodes to have
+#' @param ProngEdgeStyle a named list of the extra style values that you want the edges connected to prong nodes to have
+#' @param outf Name of the output file without any suffixes (so files will be outf.dot, outf.ps, outf.pdf, etc.)
 #' @examples
 #' # simple pedigree as marriage node diagram
-#' ped2dot(simple_test_ped(), outf = "ped2dot_ex1")
+#' junk1 <- ped2dot(simple_test_ped(), outf = "ped2dot_ex1")
 #' 
 #' # color in some of the individuals as having observed data
-#' ped2dot(simple_test_ped(), ObsNodes = c("a", "b", "f", "C", "D", "Z", "U", "10"), outf = "ped2dot_ex2")
+#' junk2 <- ped2dot(simple_test_ped(), ObsNodes = c("a", "b", "f", "C", "D", "Z", "U", "10"), outf = "ped2dot_ex2")
 #' 
 #' # draw the same thing, but make the allele freq factors invisible
-#' ped2dot(simple_test_ped(), ObsNodes = c("a", "b", "f", "C", "D", "Z", "U", "10"), 
+#' junk3 <- ped2dot(simple_test_ped(), ObsNodes = c("a", "b", "f", "C", "D", "Z", "U", "10"), 
 #'         pfactorNodeStyle = "invis", pfactorEdgeStyle = "invis",
 #'         outf = "ped2dot_ex3")
-#'         
+#'  
+#' # like example 2 but with labels on the variable nodes (big circles and squares)
+#' junk4 <- ped2dot(simple_test_ped(), ObsNodes = c("a", "b", "f", "C", "D", "Z", "U", "10"), 
+#'          outf = "ped2dot_ex4", ShowLabelNodes = unique(unlist(simple_test_ped())))
+#'          
+#' # put some prong nodes on there
+#' junk5 <- ped2dot(simple_test_ped(), ObsNodes = c("a", "b", "f", "C", "D", "Z", "U", "10"), 
+#'              outf = "ped2dot_ex5", ShowLabelNodes = unique(unlist(simple_test_ped())), ProngNodes = c("c", "d", "e", "E", "V", "3", "11", "12", "4"))
 #' @export
 ped2dot <- function(x, pa = "Pa", ma = "Ma", kid = "Kid", 
                     ObsNodes = character(0),
                     ShowLabelNodes = character(0),
+                    ProngNodes = character(0),
                     ObsStyle = list(style="filled", fillcolor="gray"),
+                    ProngStyle = list(style="dashed"),
+                    ProngEdgeStyle = list(style="dashed"),
                     outf = "pedvis-ped",
                     pfactorNodeStyle = "filled",
                     pfactorEdgeStyle = "solid"
@@ -59,6 +74,12 @@ ped2dot <- function(x, pa = "Pa", ma = "Ma", kid = "Kid",
   stopifnot(is.character(x[[kid]]))
   stopifnot(is.character(x[[pa]]))
   stopifnot(is.character(x[[ma]]))
+  
+  po_intersect <- intersect(ObsStyle, ProngStyle)
+  if(length(po_intersect) > 0) {
+    po_inter_strs <- paste(po_intersect, collapse = ", ")
+    stop(paste("Nodes cannot be ObsNodes and ProngNodes at the same time.  Problems with:", po_inter_strs))
+  }
   
   # add another column which are the names of the marriage nodes
   x$mn <- paste(x[[pa]], x[[ma]], sep="x")
@@ -70,15 +91,15 @@ ped2dot <- function(x, pa = "Pa", ma = "Ma", kid = "Kid",
   # allow function input to modify this.
   ret$pream <- c("digraph xxx {",
              "label =\"  \"",
-             "ranksep=0.4;",
-             "nodesep=0.4")
+             "ranksep=1.0;",
+             "nodesep=1.0")
                           
   
   # here we set the properties of the marriage nodes
   mn_props <- lapply(x$mn, function(z) list(shape = "circle",
                                                 style = "filled",
                                                 label = "\"\"",
-                                                height = .09,
+                                                height = 0.225,
                                                 fillcolor = "black"))
   names(mn_props) <- x$mn
   
@@ -90,16 +111,19 @@ ped2dot <- function(x, pa = "Pa", ma = "Ma", kid = "Kid",
   node_props <- lapply(node_props, function(z) list(shape = "circle", 
                                                             regular = 1,
                                                             label="\"\"",
-                                                            height = .35) )
+                                                            height = 0.86,
+                                                            fixedsize = "true") )
   
   # now, make any one listed as a dad a square (Note, I am not checking sex
   # consistency at this point.)
-  node_props[x[[pa]]] <- lapply(node_props[x[[pa]]], function(z) {z$shape = "box"; z$height = .295; z})
+  node_props[x[[pa]]] <- lapply(node_props[x[[pa]]], function(z) {z$shape = "box"; z$height = .73; z})
 
   
   # now, anyone listed as observed gets the ObsStyle applied:
   node_props[ObsNodes] <- lapply(node_props[ObsNodes], function(x) c(x, ObsStyle))
   
+  # anyone listed as a prong gets ProngStyle applied
+  node_props[ProngNodes] <- lapply(node_props[ProngNodes], function(x) c(x, ProngStyle))
   
   # and anyone listed as ShowLabel gets their label printed
   tmp  <- lapply(ShowLabelNodes, function(x) {y <- node_props[[x]]; y$label = x; y})
@@ -114,18 +138,14 @@ ped2dot <- function(x, pa = "Pa", ma = "Ma", kid = "Kid",
   ofactor_props <- lapply(ofactor_nodes, function(z) list(shape = "square",
                                             style = "filled",
                                             label = "\"\"",
-                                            height = .09,
+                                            height = 0.225,
                                             fillcolor = "black"))
   names(ofactor_props) <- ofactor_nodes
-  
-  # and here we set the text to make the ranks of the ofactors equal to their observed nodes
-  ofactor_ranks <- paste("{ rank = same; ", "\"", ofactor_nodes, "\"; ", "\"", ObsNodes, "\"; }",  sep="")
-  
+   
   
   # and finally, lets make p-factor nodes.  i.e. those nodes representing the allele frequencies
   # in the population (the prior on genotypes of founders...)
   # first, the founders are any nodes that have no parents listed.  
-  # THIS IS WHERE I AM WORKING ON THINGS AT THE MOMENT.
   mapa <- unique(c(x[[ma]], x[[pa]]))
   Founders <- mapa[!(mapa %in% x[[kid]])]
   pfactor_nodes <- paste("pf", Founders, sep="_")
@@ -133,7 +153,7 @@ ped2dot <- function(x, pa = "Pa", ma = "Ma", kid = "Kid",
                                                           style = pfactorNodeStyle,
                                                           regular = 1,
                                                           label = "\"\"",
-                                                          height = .10,
+                                                          height = 0.25,
                                                           fillcolor = "black"))
   names(pfactor_props) <- pfactor_nodes
   
@@ -143,20 +163,32 @@ ped2dot <- function(x, pa = "Pa", ma = "Ma", kid = "Kid",
   if(length(ObsNodes) > 0) {ret$ofactor_props <- prop2string(ofactor_props)}
   ret$pfactor_props <- prop2string(pfactor_props)
   
-  # store the ranks (not used now that the ofactor nodes go below the individuals)
-  # if(length(ObsNodes) > 0) ret$ofactor_ranks <- ofactor_ranks
   
   # and here we store all the text specifying edges from ofactors to observed nodes
   # note that we pump their weight way up!
   if(length(ObsNodes) > 0) {ret$of2obs_nodes <- paste("\"", ObsNodes, "\"", " -> ", "\"", ofactor_nodes, "\"", "  [dir=none, weight=10000];", sep="")}
   
   # and here is text specifying the edges connecting the pfactor nodes to the founders
-  ret$pf2founders <- paste("\"", pfactor_nodes, "\"", " -> ", "\"", Founders, "\"", "  [dir=none, style=", pfactorEdgeStyle,", weight=10000];", sep="")
+  # default edge style
+  pf2founders_style <- rep(paste("  [dir=none, weight=10000, style=", pfactorEdgeStyle,"]"), length=length(Founders))
+  # now change those for founders that are prong nodes
+  pf2founders_style[ Founders %in% ProngNodes] <- paste(" [dir = none, weight=10000,", list2keyval(ProngEdgeStyle), "]")
+  ret$pf2founders <- paste("\"", pfactor_nodes, "\"", " -> ", "\"", Founders, "\"", pf2founders_style,";", sep="")
+  
   
   # here we store all the lines for specifying edges from and to the marriage nodes
-  ret$mn2kid_arrows <- paste("\"", x$mn, "\"", " -> ", "\"", x[[kid]], "\"", "  [dir=none];", sep="")
-  ret$par2mn_arrows <- c(unique(paste("\"", x[[pa]], "\"", " -> ", "\"", x$mn, "\"", "  [dir=none];", sep="")),
-                         unique(paste("\"", x[[ma]], "\"", " -> ", "\"", x$mn, "\"", "  [dir=none];", sep="")) )
+  x$mn2kid_style = " [dir = none];"  # set the default style for this edge
+  x$par2mn_style = " [dir = none];"  # set the default style for this kind of edge
+  
+  # now, any edge connected to a prong should get additional styling
+  x$mn2kid_style[x[[kid]] %in% ProngNodes] <- paste(" [dir = none,", list2keyval(ProngEdgeStyle), "]")
+  x$par2mn_style[ (x[[ma]] %in% ProngNodes) | (x[[pa]] %in% ProngNodes)] <- paste(" [dir = none,", list2keyval(ProngEdgeStyle), "]")
+  
+  
+  
+  ret$mn2kid_arrows <- paste("\"", x$mn, "\"", " -> ", "\"", x[[kid]], "\"", x$mn2kid_style, sep="")
+  ret$par2mn_arrows <- c(unique(paste("\"", x[[pa]], "\"", " -> ", "\"", x$mn, "\"", x$par2mn_style, sep="")),
+                         unique(paste("\"", x[[ma]], "\"", " -> ", "\"", x$mn, "\"", x$par2mn_style, sep="")) )
   
   
   
@@ -165,7 +197,12 @@ ped2dot <- function(x, pa = "Pa", ma = "Ma", kid = "Kid",
   cat(unlist(ret), sep="\n", file = paste(outf, ".dot", sep=""))
   
   # I have to modify this to use outf
-  system("dot -Tps pedvis-ped.dot -o pedvis-ped.ps; epstopdf pedvis-ped.ps; open pedvis-ped.pdf")
+  DOTFile <- paste(outf, "dot", sep = ".")
+  PSFile <- paste(outf, "ps", sep = ".")
+  PDFFile <- paste(outf, "pdf", sep = ".")
+  CALL <- paste("dot -Tps", DOTFile, "-o",  PSFile, ";",  "epstopdf",  PSFile, ";",  "open",  PDFFile)
+  
+  system(CALL)
   
   # down here return more than just ret...(later...)
   ret
@@ -186,4 +223,15 @@ prop2string <- function(y) {
       paste(paste(names(z), z, sep="="), collapse = ", "),
       "];", collapse="", sep="")
   })
+}
+
+
+#' given a named list, this give a string with comma-separated keyval pairs
+#'
+#' @param z the named list
+#' @export
+#' @examples
+#' list2keyval(list(color="red", width="fat", number=2))
+list2keyval <- function(z) {
+  paste(sapply(names(z), function(y) paste(y,z[y], sep="=")), collapse = ", ")
 }
